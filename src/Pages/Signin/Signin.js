1a4c9b51c1b7dcd1b868e916cc50a56f0c6c7384
JabezSanjay <jbzsanjay2000@gmail.com>
2022-06-11 19:50:14 +0530

    Minor changes

--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -1,126 +1,138 @@
-import React from "react";
-import { Button } from "primereact/button";
-import { InputText } from "primereact/inputtext";
-import { Password } from "primereact/password";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { signinUser } from './helper';
 
 const Signin = () => {
   const {
     register,
     handleSubmit,
-    // watch,
     formState: { errors },
+    reset,
   } = useForm();
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
+  const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    // navigate("/signin");
-    console.log(data);
+  const onSubmit = async (data) => {
+    await signinUser(data, dispatch).then((response) => {
+      if (!response.success) {
+        toast.error(response.message);
+      } else {
+        toast.success('User signin successful!');
+        reset();
+        navigate(`${response.data.role}/dashboard`);
+      }
+    });
   };
 
-  // console.log(watch("username"));
-
   return (
-    <div className="bg-gray-700 h-screen">
-      <div className="lg:pt-24">
-        <div className="overflow-hidden">
-          <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-            <div className="flex flex-col items-center justify-between xl:flex-row">
-              <div className="w-full max-w-xl mb-12 xl:pr-16 xl:mb-0 xl:w-7/12">
-                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                  The quick, brown fox <br className="hidden md:block" />
-                  jumps over a{" "}
-                  <span className="text-teal-accent-400">lazy dog</span>
+    <div className='bg-gray-700 h-screen'>
+      <div className='lg:pt-24'>
+        <div className='overflow-hidden'>
+          <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
+            <div className='flex flex-col items-center justify-between xl:flex-row'>
+              <div className='w-full max-w-xl mb-12 xl:pr-16 xl:mb-0 xl:w-7/12'>
+                <h2 className='max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none'>
+                  The quick, brown fox <br className='hidden md:block' />
+                  jumps over a{' '}
+                  <span className='text-teal-accent-400'>lazy dog</span>
                 </h2>
-                <p className="max-w-xl mb-4 text-base text-gray-400 md:text-lg">
+                <p className='max-w-xl mb-4 text-base text-gray-400 md:text-lg'>
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudan, totam rem aperiam, eaque ipsa
                   quae.
                 </p>
               </div>
-              <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
-                <div className="relative">
+              <div className='w-full max-w-xl xl:px-8 xl:w-5/12'>
+                <div className='relative'>
                   <svg
-                    viewBox="0 0 52 24"
-                    fill="currentColor"
-                    className="absolute bottom-0 right-0 z-0 hidden w-32 -mb-8 -mr-20 text-teal-accent-400 lg:w-32 lg:-mr-16 sm:block"
+                    viewBox='0 0 52 24'
+                    fill='currentColor'
+                    className='absolute bottom-0 right-0 z-0 hidden w-32 -mb-8 -mr-20 text-teal-accent-400 lg:w-32 lg:-mr-16 sm:block'
                   >
                     <defs>
                       <pattern
-                        id="766323e1-e594-4ffd-a688-e7275079d540"
-                        x="0"
-                        y="0"
-                        width=".135"
-                        height=".30"
+                        id='766323e1-e594-4ffd-a688-e7275079d540'
+                        x='0'
+                        y='0'
+                        width='.135'
+                        height='.30'
                       >
-                        <circle cx="1" cy="1" r=".7" />
+                        <circle cx='1' cy='1' r='.7' />
                       </pattern>
                     </defs>
                     <rect
-                      fill="url(#766323e1-e594-4ffd-a688-e7275079d540)"
-                      width="52"
-                      height="24"
+                      fill='url(#766323e1-e594-4ffd-a688-e7275079d540)'
+                      width='52'
+                      height='24'
                     />
                   </svg>
-                  <div className="relative bg-white rounded shadow-2xl p-7 sm:p-10">
-                    <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+                  <div className='relative bg-white rounded shadow-2xl p-7 sm:p-10'>
+                    <h3 className='mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl'>
                       Sign In
                     </h3>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                      <div className="mb-1 sm:mb-2">
+                      <div className='mb-1 sm:mb-2'>
                         <label
-                          htmlFor="email"
-                          className="inline-block mb-1 font-medium"
+                          htmlFor='email'
+                          className='inline-block mb-1 font-medium'
                         >
                           E-mail
                         </label>
                         <InputText
-                          placeholder="john.doe@example.org"
-                          {...register("email", {
-                            required: "Email is required",
+                          placeholder='john.doe@example.org'
+                          {...register('email', {
+                            required: 'Email is required',
                           })}
-                          style={{ width: "100%" }}
+                          style={{ width: '100%' }}
                         />
                         <p
-                          style={{ fontSize: "15px" }}
-                          className="mt-1 text-red-400"
+                          style={{ fontSize: '15px' }}
+                          className='mt-1 text-red-400'
                         >
                           {errors.email?.message}
                         </p>
                       </div>
-                      <div className="mb-1 sm:mb-2">
+                      <div className='mb-1 sm:mb-2'>
                         <label
-                          htmlFor="password"
-                          className="inline-block mb-1 font-medium"
+                          htmlFor='password'
+                          className='inline-block mb-1 font-medium'
                         >
                           Password
                         </label>
-                        <Password
-                          placeholder="Enter a new password"
-                          {...register("password", {
-                            required: "Password is required",
+                        <InputText
+                          type='password'
+                          placeholder='Enter a new password'
+                          {...register('password', {
+                            required: 'Password is required',
                           })}
-                          className="w-full"
+                          className='w-full'
                           feedback={false}
                         />
                         <p
-                          style={{ fontSize: "15px" }}
-                          className="mt-1 text-red-400"
+                          style={{ fontSize: '15px' }}
+                          className='mt-1 text-red-400'
                         >
                           {errors.password?.message}
                         </p>
                       </div>
-                      <div className="mt-4 mb-2 sm:mb-4">
+                      <div className='mt-4 mb-2 sm:mb-4'>
                         <Button
-                          style={{ width: "100%" }}
-                          type="submit"
-                          label="Submit"
-                          aria-label="Submit"
+                          style={{ width: '100%' }}
+                          type='submit'
+                          label='Submit'
+                          aria-label='Submit'
+                          loading={loading}
                         />
                       </div>
-                      <p className="text-xs text-gray-600 sm:text-sm">
+                      <p className='text-xs text-gray-600 sm:text-sm'>
                         Dont have an account?
-                        <Link className="text-blue-700" to="/signin">
+                        <Link className='text-blue-700' to='/signin'>
                           Sign Up
                         </Link>
                       </p>
--- src/Router.js
+++ src/Router.js
@@ -1,29 +1,88 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Admin from "./Pages/Admin/User";
-import ChatwithMentor from "./Pages/Common/ChatwithMentor";
-import TicketTracking from "./Pages/Common/TrackingTicket";
-import Signin from "./Pages/Signin/Signin";
-import Signup from "./Pages/Signup/Signup";
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+  Outlet,
+} from 'react-router-dom';
+import Admin from './Pages/Admin/User';
+import ChatwithMentor from './Pages/Common/ChatwithMentor';
+import TicketTracking from './Pages/Common/TrackingTicket';
+import Signin from './Pages/Signin/Signin';
+import Signup from './Pages/Signup/Signup';
+import { ToastContainer } from 'react-toastify';
+import { useSelector } from 'react-redux';
 
 function Router() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/admin/users" element={<Admin />} />
-          <Route path="/admin/tickettracking" element={<TicketTracking />} />
-          <Route path="/user/chat" element={<ChatwithMentor />} />
-          <Route path="/" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          {/* <Route element={<LoggedInUsers />}>
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/signup" element={<Signup />} />
-          </Route> */}
+          <Route path='/' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route element={<AdminRoute />}>
+            <Route path='/admin/users' element={<Admin />} />
+            <Route path='/admin/tickettracking' element={<TicketTracking />} />
+          </Route>
+          <Route element={<StudentRoute />}>
+            <Route path='/user/chat' element={<ChatwithMentor />} />
+          </Route>
         </Routes>
       </BrowserRouter>
+      <ToastContainer
+        position='bottom-right'
+        autoClose={3100}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      ></ToastContainer>
     </div>
   );
+}
+
+function AvoidCreateOrSignin() {
+  const auth = useSelector((state) => state.auth);
+  let location = useLocation();
+
+  if (auth.isLoggedIn) {
+    return <Navigate to='/' state={{ from: location }} />;
+  }
+  return <Outlet />;
+}
+
+function StudentRoute() {
+  const auth = useSelector((state) => state.auth);
+  let location = useLocation();
+
+  if (!auth.isLoggedIn || auth.userInfo.role !== 'student') {
+    return <Navigate to='/' state={{ from: location }} />;
+  }
+  return <Outlet />;
+}
+
+function MentorRoute() {
+  const auth = useSelector((state) => state.auth);
+  let location = useLocation();
+
+  if (!auth.isLoggedIn || auth.userInfo.role !== 'mentor') {
+    return <Navigate to='/' state={{ from: location }} />;
+  }
+  return <Outlet />;
+}
+
+function AdminRoute() {
+  const auth = useSelector((state) => state.auth);
+  let location = useLocation();
+
+  if (!auth.isLoggedIn || auth.userInfo.role !== 'admin') {
+    return <Navigate to='/' state={{ from: location }} />;
+  }
+  return <Outlet />;
 }
 
 export default Router;
