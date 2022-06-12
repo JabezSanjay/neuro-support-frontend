5225821e81491ce6106daf439626b5606fc27bed
JabezSanjay <jbzsanjay2000@gmail.com>
2022-06-12 07:00:52 +0530

    Fixes

--- src/Pages/Mentor/Tickets.js
+++ src/Pages/Mentor/Tickets.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MentorTicketTable from '../../Components/AppAdminTable/MentorTicketTable';
 import AppSideNavbar from '../../Components/AppSideBar/AppSidebar';
 import Navbar from '../../Components/Navbar/Navbar';
+import axios from '../../axiosConfig';
 
 const Tickets = () => {
+  const readTickets = async () => {};
+  useEffect(() => {}, []);
   return (
     <div>
       <AppSideNavbar>
         <Navbar />
+        <div className='p-20'>
+          <MentorTicketTable />
+        </div>
       </AppSideNavbar>
     </div>
   );
